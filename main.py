706ba24763cffc7dd780361a00e207ac69d4bdb2
itsusony <meng.xiangliang1985@gmail.com>
2024-04-24 03:30:01 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import gradio as gr
+import transformers
+import torch
+
+MODEL_ID = "haqishen/Llama-3-8B-Japanese-Instruct"
+MAX_MEMORY_WORDS = 8192 
+MAX_NEW_TOKENS = 8192
+TEMPERATURE = 0.6
+TOP_P = 0.9
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=MODEL_ID,
+    model_kwargs={"torch_dtype": torch.bfloat16},
+    device="cuda",
+)
+
+def chat(message, history):
+    if not message:
+        return None
+
+    messages = []
+    memory_len = 0
+    for h in history:
+        req = h[0]
+        messages.append({"role": "user", "content": req})
+        memory_len += len(req)
+        if memory_len >= MAX_MEMORY_WORDS:
+            break
+    messages.append({"role": "user", "content": message})
+
+    prompt = pipeline.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    terminators = [
+        pipeline.tokenizer.eos_token_id,
+        pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+    ]
+    
+    outputs = pipeline(
+        prompt,
+        max_new_tokens=MAX_NEW_TOKENS,
+        eos_token_id=terminators,
+        do_sample=True,
+        temperature=TEMPERATURE,
+        top_p=TOP_P,
+    )
+    return outputs[0]["generated_text"][len(prompt):]
+
+gr.ChatInterface(chat).launch(share=True)
