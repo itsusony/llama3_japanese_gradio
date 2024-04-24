f00215e7567cf6024a41bb8d79e596864479c3aa
itsusony <meng.xiangliang1985@gmail.com>
2024-04-24 12:48:31 +0900

    add slider

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import torch
 
 MODEL_ID = "haqishen/Llama-3-8B-Japanese-Instruct"
 MAX_MEMORY_WORDS = 8192
-MAX_NEW_TOKENS = 8192
-TEMPERATURE = 0.6
-TOP_P = 0.9
 
 pipeline = transformers.pipeline(
     "text-generation",
@@ -15,14 +12,25 @@ pipeline = transformers.pipeline(
     device="cuda",
 )
 
-def chat(message, history):
+def chat(message, history, system_role, token, temperature, top_p):
     if not message:
-        return None
+        return history, ""
 
-    messages = [{"role": "user", "content": h[0]} for h in history]
+    messages = []
+    if system_role:
+        messages.append({"role": "system", "content": system_role})
+
+    memory_len = 0
+    for h in history:
+        req = h[0]
+        messages.append({"role": "user", "content": req})
+        memory_len += len(req)
+        if memory_len >= MAX_MEMORY_WORDS:
+            break
+
     messages.append({"role": "user", "content": message})
-
     prompt = pipeline.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+
     terminators = [
         pipeline.tokenizer.eos_token_id,
         pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
@@ -30,13 +38,31 @@ def chat(message, history):
 
     outputs = pipeline(
         prompt,
-        max_new_tokens=MAX_NEW_TOKENS,
+        max_new_tokens=token,
         eos_token_id=terminators,
         do_sample=True,
-        temperature=TEMPERATURE,
-        top_p=TOP_P,
+        temperature=temperature,
+        top_p=top_p,
     )
 
-    return outputs[0]["generated_text"][len(prompt):]
+    response = outputs[0]["generated_text"][len(prompt):]
+    history.append((message, response))
+    return history, ""
 
-gr.ChatInterface(chat).launch(share=True)
+with gr.Blocks() as webapp:
+    with gr.Row():
+        with gr.Column():
+            system_role = gr.Textbox("", label="System Role", lines=5)
+        with gr.Column():
+            token = gr.Slider(128, 1024*100, 8192, step=128, label="Max New Token")
+            top_p = gr.Slider(0, 1, 0.9, step=0.1, label="top_p")
+            temperature = gr.Slider(0, 1, 0.6, step=0.1, label="temperature")
+
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox(value="", label="Enter your message")
+    clear = gr.Button("Clear")
+
+    msg.submit(chat, [msg, chatbot, system_role, token, temperature, top_p], [chatbot, msg])
+    clear.click(lambda: [], None, chatbot, queue=False)
+
+webapp.launch(share=True)
